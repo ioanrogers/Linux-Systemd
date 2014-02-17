3b961375c84fb29e5e7a05e420cdbe8a272ee0cd
Ioan Rogers <ioanr@cpan.org>
2014-02-16 18:26:22 -0800

    [doc] updated example code

--- eg/write_journal.pl
+++ eg/write_journal.pl
@@ -4,21 +4,11 @@ use v5.10.1;
 use strict;
 use warnings;
 use Linux::Systemd::Journal::Write;
-use Data::Printer;
 
-my $jnl = Linux::Systemd::Journal::Write->new(
-    id => 'blah',
-);
+my $jnl = Linux::Systemd::Journal::Write->new;
 
-#$jnl->print('flarg');
-#$jnl->print(['Fuck %s', 'you'], 4);
+$jnl->print('flarg');
+$jnl->print('Hello world', 4);
 
-my $data = {
-    message => 'TESTING',
-    abstract => 'XS wrapper around sd-journal',
-    author => 'Ioan Rogers <ioanr@cpan.org>',
-    dynamic_config => 0,
-    generated_by => 'Dist::Zilla version 4.300037, CPAN::Meta::Converter version 2.132140',
-};
-
-$jnl->send($data);
+my %hash = (1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday');
+$jnl->send('Here is a message', \%hash);
