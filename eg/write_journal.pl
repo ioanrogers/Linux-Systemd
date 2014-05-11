80158c1eebf2b4f0372215c917a9689909292291
Ioan Rogers <ioanr@cpan.org>
2014-05-11 12:55:25 -0700

    Update example

--- eg/write_journal.pl
+++ eg/write_journal.pl
@@ -10,5 +10,8 @@ my $jnl = Linux::Systemd::Journal::Write->new;
 $jnl->print('flarg');
 $jnl->print('Hello world', 4);
 
-my %hash = (1 => 'Monday', 2 => 'Tuesday', 3 => 'Wednesday');
+$jnl->perror(127);
+
+my %hash =
+  (DAY_ONE => 'Monday', DAY_TWO => 'Tuesday', DAY_THREE => 'Wednesday');
 $jnl->send('Here is a message', \%hash);
