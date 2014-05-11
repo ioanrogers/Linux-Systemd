4342be8ffffe7c9f16cda6ca96b1c1488c3e7b23
Ioan Rogers <ioanr@cpan.org>
2014-05-11 12:54:35 -0700

    Also suggest Fedora dev package

--- inc/My/Builder.pm
+++ inc/My/Builder.pm
@@ -19,7 +19,7 @@ sub new {
         %pkg_info = ExtUtils::PkgConfig->find($pkg_name);
     }
     catch {
-        say "Do you need to install libsystemd-journal-dev?";
+        say "Do you need to install libsystemd-journal-dev (debian) or systemd-devel (fedora)?";
         exit;
     };
 
