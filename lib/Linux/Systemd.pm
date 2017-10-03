8aa6aa48264043f0f5ecad89d70ee4bc2461aa39
Ioan Rogers <ioanr@cpan.org>
2017-10-02 21:11:30 -0700

    Fix entrypoint POD

--- lib/Linux/Systemd.pm
+++ lib/Linux/Systemd.pm
@@ -2,14 +2,20 @@ package Linux::Systemd;
 
 use v5.16;
 
-# ABSTRACT: Use the systemd journal
+# ABSTRACT: Bindings for C<systemd> APIs
 
 1;
 
 =head1 DESCRIPTION
+
+The following C<systemd> components are wrapped to some to degree.
 
 =head2 Journal
 
 To log to the journal, see L<Linux::Systemd::Journal::Write>.
 
 To read from the journal, see L<Linux::Systemd::Journal::Read>.
+
+=head2 Daemon
+
+To report status and use service watchdogs, see L<Linux::Systemd::Daemon>.
